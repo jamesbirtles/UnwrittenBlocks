c81ff4306806d9450a7b92cf9fc40f18d6e1988d
UnwrittenFun <unwrittenfun@gmail.com>
2013-11-24 17:23:23 +0000

    Whoops

--- WallTeleporters/src/unwrittenfun/minecraft/wallteleporters/CommonProxy.java
+++
@@ -1,9 +0,0 @@
-package unwrittenfun.minecraft.wallteleporters;
-
-/**
- * Mod: Wall Teleporters
- * Author: UnwrittenFun
- * License: Minecraft Mod Public License (Version 1.0.1)
- */
-public class CommonProxy {
-}
--- WallTeleporters/src/unwrittenfun/minecraft/wallteleporters/handlers/EventHandler.java
+++
@@ -1,9 +0,0 @@
-package unwrittenfun.minecraft.wallteleporters.handlers;
-
-/**
- * Mod: Wall Teleporters
- * Author: UnwrittenFun
- * License: Minecraft Mod Public License (Version 1.0.1)
- */
-public class EventHandler {
-}
