74cb6b325f9dfe586d04103feccf58084b801f18
James Birtles <unwrittenfun@gmail.com>
2014-01-19 21:00:52 +0000

    Disable WIP block cutter for release

--- src/unwrittenfun/minecraft/unwrittenblocks/blocks/UBBlocks.scala
+++ src/unwrittenfun/minecraft/unwrittenblocks/blocks/UBBlocks.scala
@@ -32,12 +32,12 @@ object UBBlocks {
   def registerNames() {
     LanguageRegistry.addName(pleatherPlant, PLEATHER_PLANT_NAME)
     LanguageRegistry.addName(wallTeleporter, WALL_TELEPORTER_NAME)
-    LanguageRegistry.addName(blockCutter, CUTTER_NAME)
+    //LanguageRegistry.addName(blockCutter, CUTTER_NAME)
   }
 
   def registerTileEntities() {
     GameRegistry.registerTileEntity(classOf[TileEntityWallTeleporter], WALL_TELEPORTER_KEY)
-    GameRegistry.registerTileEntity(classOf[TileEntityBlockCutter], CUTTER_KEY)
+    //GameRegistry.registerTileEntity(classOf[TileEntityBlockCutter], CUTTER_KEY)
   }
 
   def registerRecipes() {
@@ -46,18 +46,18 @@ object UBBlocks {
     // Register Vanilla Cutter Recipes
 
     // Stairs
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 0), (Block.stairsWoodOak.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 1), (Block.stairsWoodSpruce.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 2), (Block.stairsWoodBirch.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 3), (Block.stairsWoodJungle.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.cobblestone.blockID, 0), (Block.stairsCobblestone.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.brick.blockID, 0), (Block.stairsBrick.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.stoneBrick.blockID, 0), (Block.stairsStoneBrick.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.netherBrick.blockID, 0), (Block.stairsNetherBrick.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.sandStone.blockID, 0), (Block.stairsSandStone.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.sandStone.blockID, 1), (Block.stairsSandStone.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.sandStone.blockID, 2), (Block.stairsSandStone.blockID, 0))
-    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.blockNetherQuartz.blockID, 0), (Block.stairsNetherQuartz.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 0), (Block.stairsWoodOak.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 1), (Block.stairsWoodSpruce.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 2), (Block.stairsWoodBirch.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.planks.blockID, 3), (Block.stairsWoodJungle.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.cobblestone.blockID, 0), (Block.stairsCobblestone.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.brick.blockID, 0), (Block.stairsBrick.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.stoneBrick.blockID, 0), (Block.stairsStoneBrick.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.netherBrick.blockID, 0), (Block.stairsNetherBrick.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.sandStone.blockID, 0), (Block.stairsSandStone.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.sandStone.blockID, 1), (Block.stairsSandStone.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.sandStone.blockID, 2), (Block.stairsSandStone.blockID, 0))
+//    CutterRecipes.registerRecipe(CutterRecipes.MODE_STAIRS, (Block.blockNetherQuartz.blockID, 0), (Block.stairsNetherQuartz.blockID, 0))
 
     // Slabs TODO
   }
