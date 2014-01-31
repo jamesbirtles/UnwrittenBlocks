fb37940dd69d6e77a1a571bd541dbae6d748d519
James Birtles <unwrittenfun@gmail.com>
2014-01-31 18:59:52 +0000

    Version 0.3.0

--- scala/unwrittenfun/minecraft/unwrittenblocks/package.scala
+++ scala/unwrittenfun/minecraft/unwrittenblocks/package.scala
@@ -8,7 +8,7 @@ package unwrittenfun.minecraft
 package object unwrittenblocks {
   final val ID = "UnwrittenBlocks"
   final val NAME = "UnwrittenBlocks"
-  final val VERSION = "0.2.0"
+  final val VERSION = "0.3.0"
   final val CHANNEL = "unwrittenBlocks"
   final val TEXTURE_LOCATION = "unwrittenblocks"
 }
