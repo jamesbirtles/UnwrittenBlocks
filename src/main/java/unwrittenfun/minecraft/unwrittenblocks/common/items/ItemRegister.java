3651d90ca17bfc9267eff479ef951ec76515718f
UnwrittenFun <unwrittenfun@gmail.com>
2014-08-29 20:20:44 +0100

    Add storage balls

--- src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/items/ItemRegister.java
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/items/ItemRegister.java
@@ -10,10 +10,13 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ItemRegister {
   public static final ItemUB gpsChip = new ItemGPSChip();
+  public static final ItemUB storageBall = new ItemStorageBall();
 
   public static void registerItems() {
     GameRegistry.registerItem(gpsChip, GPS_CHIP_KEY);
+    GameRegistry.registerItem(storageBall, STORAGE_BALL_KEY);
   }
 
   public static final String GPS_CHIP_KEY = "gpsChip";
+  public static final String STORAGE_BALL_KEY = "storageBall";
 }
---
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/items/ItemStorageBall.java
@@ -0,0 +1,60 @@
+package unwrittenfun.minecraft.unwrittenblocks.common.items;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.StatCollector;
+
+import java.beans.Introspector;
+import java.util.List;
+
+/**
+ * Project: UnwrittenBlocks
+ * Author: UnwrittenFun
+ * Created: 29/08/2014.
+ */
+public class ItemStorageBall extends ItemUB {
+  public static final String[] BALL_TYPES = new String[] {
+      "Cobblestone", "MossyCobblestone", "Netherrack", "OakWood"
+  };
+
+  public ItemStorageBall() {
+    super(ItemRegister.STORAGE_BALL_KEY);
+
+    setHasSubtypes(true);
+  }
+
+  private IIcon[] icons;
+
+  @Override
+  public void registerIcons(IIconRegister iconRegister) {
+    icons = new IIcon[BALL_TYPES.length];
+    for (int i = 0; i < BALL_TYPES.length; i++) {
+      icons[i] = iconRegister.registerIcon(getIconString() + BALL_TYPES[i]);
+    }
+  }
+
+  @Override
+  public IIcon getIconFromDamage(int meta) {
+    return meta < icons.length ? icons[meta] : icons[0];
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void getSubItems(Item item, CreativeTabs stack, List list) {
+    for (int i = 0; i < BALL_TYPES.length; i++) {
+      list.add(new ItemStack(this, 1, i));
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean bool) {
+    super.addInformation(stack, player, lines, bool);
+
+    lines.add(StatCollector.translateToLocal("item." + ItemRegister.STORAGE_BALL_KEY + "." + Introspector.decapitalize(BALL_TYPES[stack.getItemDamage()]) + ".name"));
+  }
+}
