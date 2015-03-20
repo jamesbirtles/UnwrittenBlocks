f10268ab48eaeb0cf13ef4226ff2fc9bc9ff9a29
UnwrittenFun <unwrittenfun@gmail.com>
2015-03-20 17:37:05 +0000

    Add nei integration

--- src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/blocks/BlockRegister.java
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/blocks/BlockRegister.java
@@ -1,6 +1,10 @@
 package unwrittenfun.minecraft.unwrittenblocks.common.blocks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import unwrittenfun.minecraft.unwrittenblocks.common.recipes.InfuserRecipes;
 import unwrittenfun.minecraft.unwrittenblocks.common.tileEntities.TEDarkInfuser;
 
 /**
@@ -8,10 +12,10 @@ import unwrittenfun.minecraft.unwrittenblocks.common.tileEntities.TEDarkInfuser;
  */
 public class BlockRegister {
   public static final String PLEATHER_PLANT_KEY = "pleatherPlant";
-  public static final String DARK_INFUSER_KEY   = "darkInfuser";
+  public static final String DARK_INFUSER_KEY = "darkInfuser";
 
   public static BlockPleatherPlant pleatherPlant;
-  public static BlockDarkInfuser   darkInfuser;
+  public static BlockDarkInfuser darkInfuser;
 
   public static void registerBlocks() {
     pleatherPlant = new BlockPleatherPlant(PLEATHER_PLANT_KEY);
@@ -21,5 +25,11 @@ public class BlockRegister {
     GameRegistry.registerBlock(darkInfuser, DARK_INFUSER_KEY);
 
     GameRegistry.registerTileEntity(TEDarkInfuser.class, "TE" + DARK_INFUSER_KEY);
+  }
+
+  public static void registerRecipes() {
+    GameRegistry.addRecipe(new ItemStack(darkInfuser, 1), "ooo", "dnd", "nnn", 'o', Blocks.obsidian, 'd', Items.diamond, 'n', Blocks.nether_brick);
+
+    InfuserRecipes.instance.addRecipe(new ItemStack(Blocks.stone), new ItemStack(Blocks.end_stone), 1000);
   }
 }
---
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/nei/NEIUnwrittenBlocksConfig.java
@@ -0,0 +1,26 @@
+package unwrittenfun.minecraft.unwrittenblocks.common.nei;
+
+import codechicken.nei.api.API;
+import codechicken.nei.api.IConfigureNEI;
+import unwrittenfun.minecraft.unwrittenblocks.common.ModInfo;
+
+/**
+ * Author: James Birtles
+ */
+public class NEIUnwrittenBlocksConfig implements IConfigureNEI {
+  @Override
+  public void loadConfig() {
+    API.registerRecipeHandler(new InfuserRecipeHandler());
+    API.registerUsageHandler(new InfuserRecipeHandler());
+  }
+
+  @Override
+  public String getName() {
+    return "UnwrittenBlocks Plugin";
+  }
+
+  @Override
+  public String getVersion() {
+    return ModInfo.VERSION;
+  }
+}
