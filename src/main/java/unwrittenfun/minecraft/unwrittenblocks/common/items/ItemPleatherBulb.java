047138511f3f5c88d1e12130943af9985cf5162f
UnwrittenFun <unwrittenfun@gmail.com>
2015-03-26 21:45:43 +0000

    Reduce stack size by one when placing bulb

--- src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/items/ItemPleatherBulb.java
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/items/ItemPleatherBulb.java
@@ -21,32 +21,37 @@ public class ItemPleatherBulb extends ItemUB implements IPlantable {
     super(key);
   }
 
-  @Override public EnumPlantType getPlantType(IBlockAccess world, int x, int y, int z) {
+  @Override
+  public EnumPlantType getPlantType(IBlockAccess world, int x, int y, int z) {
     return EnumPlantType.Plains;
   }
 
-  @Override public Block getPlant(IBlockAccess world, int x, int y, int z) {
+  @Override
+  public Block getPlant(IBlockAccess world, int x, int y, int z) {
     return BlockRegister.pleatherPlant;
   }
 
-  @Override public int getPlantMetadata(IBlockAccess world, int x, int y, int z) {
+  @Override
+  public int getPlantMetadata(IBlockAccess world, int x, int y, int z) {
     return 0;
   }
 
-  @Override public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side,
-                                     float hitX, float hitY, float hitZ) {
-    if (!world.isRemote) {
+  @Override
+  public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
       Block block = world.getBlock(x, y, z);
-      if (world.isAirBlock(x, y + 1, z) && (block == Blocks.grass || block == Blocks.dirt)) {
+      if (side == 1 && world.isAirBlock(x, y + 1, z) && (block == Blocks.grass || block == Blocks.dirt)) {
+        stack.stackSize--;
         world.setBlock(x, y + 1, z, BlockRegister.pleatherPlant);
+
         return true;
       }
-    }
     return false;
   }
 
-  @SuppressWarnings("unchecked") @Override public void addInformation(ItemStack stack, EntityPlayer player, List lines,
-                                                                      boolean bool) {
+  @SuppressWarnings("unchecked")
+  @Override
+  public void addInformation(ItemStack stack, EntityPlayer player, List lines,
+                             boolean bool) {
     Collections.addAll(lines, getLinesFromLang("unwrittenblocks.text.pleatherBulb"));
 
     super.addInformation(stack, player, lines, bool);
