d72e84b531d8806dedf1a8c2ad7b15db2e18399c
UnwrittenFun <unwrittenfun@gmail.com>
2015-03-27 14:01:06 +0000

    Clean up the icon generation code a little

--- src/main/java/unwrittenfun/minecraft/unwrittenblocks/client/resourcepack/ExternalResourcePack.java
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/client/resourcepack/ExternalResourcePack.java
@@ -8,12 +8,14 @@ import java.io.File;
  * Author: James Birtles
  */
 public class ExternalResourcePack extends FolderResourcePack {
-  public ExternalResourcePack(File folder) {
+  public String name;
+  public ExternalResourcePack(String name, File folder) {
     super(folder);
+    this.name = name;
   }
 
   @Override
   public String getPackName() {
-    return "unwrittenblocks";
+    return name;
   }
 }
