c2b5b3164eb74bd525e5d2f781e0398e14f7735b
UnwrittenFun <unwrittenfun@gmail.com>
2015-03-30 15:31:15 +0100

    Bump to version 2.1.0

--- src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/ModInfo.java
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/ModInfo.java
@@ -6,6 +6,6 @@ package unwrittenfun.minecraft.unwrittenblocks.common;
 public class ModInfo {
   public static final String MOD_ID = "UnwrittenBlocks";
   public static final String NAME = "Unwritten Blocks";
-  public static final String VERSION = "1.7.10-2.0.1";
+  public static final String VERSION = "1.7.10-2.1.0";
   public static final String RESOURCE_LOCATION = "unwrittenblocks";
 }
