3e72627be2a87d169081316977dabba35f783786
UnwrittenFun <unwrittenfun@gmail.com>
2015-05-24 17:50:58 +0100

    Version should sync from gradle on build

--- src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/ModInfo.java
+++ src/main/java/unwrittenfun/minecraft/unwrittenblocks/common/ModInfo.java
@@ -6,6 +6,6 @@ package unwrittenfun.minecraft.unwrittenblocks.common;
 public class ModInfo {
   public static final String MOD_ID = "UnwrittenBlocks";
   public static final String NAME = "Unwritten Blocks";
-  public static final String VERSION = "1.7.10-2.2.0-pre";
+  public static final String VERSION = "@VERSION@";
   public static final String RESOURCE_LOCATION = "unwrittenblocks";
 }
